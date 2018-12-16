d35a2f0d96f36d119f797be4ae425c3f678b7c09
belerico <belo.fede@outlook.com>
2018-12-16 15:39:03 +0100

    Added getter and setter for User class

--- src/main/java/com/assignment3/jpa/models/User.java
+++ src/main/java/com/assignment3/jpa/models/User.java
@@ -3,7 +3,6 @@ package com.assignment3.jpa.models;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import java.util.Date;
 
 @Entity
 public class User {
@@ -15,20 +14,6 @@ public class User {
     private String surname;
     private String password;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", password='" + password + '\'' +
-                ", email='" + email + '\'' +
-                ", dateOfBirth=" + dateOfBirth +
-                '}';
-    }
-
-    private String email;
-    private Date dateOfBirth;
 
     public Long getId() {
         return id;
@@ -61,21 +46,4 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
 }
