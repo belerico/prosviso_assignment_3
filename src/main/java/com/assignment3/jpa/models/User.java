5b6426004f136c27b0bbb834f921452cee875077
belerico <belo.fede@outlook.com>
2018-12-16 15:21:22 +0100

    Added getter and setter for User class

--- src/main/java/com/assignment3/jpa/models/User.java
+++ src/main/java/com/assignment3/jpa/models/User.java
@@ -17,68 +17,28 @@ public class User {
     private String email;
     private Date dateOfBirth;
 
-    /**
-     * @return the name
-     */
-    public String getName() {
-        return name;
-    }
-
-    /**
-     * @param name the name to set
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    /**
-     * @return the surname
-     */
-    public String getSurname() {
-        return surname;
-    }
-
-    /**
-     * @param surname the surname to set
-     */
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    /**
-     * @return the email
-     */
-    public String getEmail() {
-        return email;
-    }
-
-    /**
-     * @param email the email to set
-     */
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    /**
-     * @return the date of birth
-     */
-    public Date dateOfBirth() {
-        return dateOfBirth;
-    }
-
-    /**
-     * @param dateOfBirth of birth the dateOfBirth to set
-     */
-    public void setBirthdate(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
     public Long getId() {
         return id;
     }
 
-    void setId(Long id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getPassword() {
@@ -89,12 +49,20 @@ public class User {
         this.password = password;
     }
 
-    /* (non-Javadoc)
-     * @see java.lang.Object#toString()
-     */
-    @Override
-    public String toString() {
-        return "User [id=" + id + ", name=" + name + ", surname=" + surname + ", password=" + password + ", email="
-                + email + ", birthdate=" + dateOfBirth + "]";
+    public String getEmail() {
+        return email;
     }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
 }
