3929c6f7038140d3b5d21d740c9acfc527308e64
c.carta4 <c.carta4@campus.unimib.it>
2018-12-17 10:54:22 +0100

    Added attributes to Place Class, ToString, Getter and Setter

--- src/main/java/com/assignment3/jpa/models/Place.java
+++ src/main/java/com/assignment3/jpa/models/Place.java
@@ -1,4 +1,61 @@
 package com.assignment3.jpa.models;
 
-public class Place {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Entity
+public class Place
+{
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String citta;
+    private String provincia;
+    private String regione;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public String getRegione() {
+        return regione;
+    }
+
+    public void setRegione(String regione) {
+        this.regione = regione;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Place{" +
+                "id=" + id +
+                ", citta='" + citta + '\'' +
+                ", provincia='" + provincia + '\'' +
+                ", regione='" + regione + '\'' +
+                '}';
+    }
 }
