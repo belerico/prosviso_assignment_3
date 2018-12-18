9b1265cfd3dbabbb739d6cfc228fec635e2a116e
belerico <belo.fede@outlook.com>
2018-12-18 22:51:33 +0100

    Added add/removePlace to User and BusinessActivity class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -60,12 +60,11 @@ public class Main {
         entityManager.getTransaction().commit();
 
         entityManager.getTransaction().begin();
-        /*user1.removeStandardCard(sc2);
+        user1.removeStandardCard(sc2);
         user2.removeStandardCard(sc3);
-        place1.removeUser(user1);
-        place2.addUser(user2);
-        place1.removeBusinessActivity(ba2);*/
-        user1.removeSharableCard(sh1);
+        user1.removePlace(place1);
+        user1.removeSharableCard(user2, sh1);
+        ba1.removePlace(place1);
         entityManager.getTransaction().commit();
     }
 }
