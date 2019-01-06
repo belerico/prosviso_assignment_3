9e101c9ab197573d6a6682923267cb68c5d99257
belerico <belo.fede@outlook.com>
2019-01-06 02:40:23 +0100

    Refactor services

--- src/main/java/com/assignment3/jpa/service/PlaceService.java
+++ src/main/java/com/assignment3/jpa/service/PlaceService.java
@@ -1,21 +1,51 @@
 package com.assignment3.jpa.service;
 
 import com.assignment3.jpa.dao.PlaceDao;
+import com.assignment3.jpa.model.BusinessActivity;
 import com.assignment3.jpa.model.Place;
+import com.assignment3.jpa.model.User;
 
 public class PlaceService extends AbstractService<Place, Long> {
 
     public PlaceService() { super(new PlaceDao()); }
 
-    public void removeAllActivities(Place place) {
+    public void addUser(Place place, User user) {
         getDao().begin();
-        place.removeAllActivities();
+        place.addUser(user);
+        getDao().commit();
+    }
+
+    public void removeUser(User user) {
+        Place place = user.getPlace();
+        getDao().begin();
+        if (place != null)
+            place.removeUser(user);
         getDao().commit();
     }
 
     public void removeAllUser(Place place) {
         getDao().begin();
         place.removeAllUser();
+        getDao().commit();
+    }
+
+    public void addBusinessActivity(Place place, BusinessActivity businessActivity) {
+        getDao().begin();
+        place.addBusinessActivity(businessActivity);
+        getDao().commit();
+    }
+
+    public void removeBusinessActivity(BusinessActivity businessActivity) {
+        Place place = businessActivity.getPlace();
+        getDao().begin();
+        if (place != null)
+            place.removeBusinessActivity(businessActivity);
+        getDao().commit();
+    }
+
+    public void removeAllActivities(Place place) {
+        getDao().begin();
+        place.removeAllActivities();
         getDao().commit();
     }
 
